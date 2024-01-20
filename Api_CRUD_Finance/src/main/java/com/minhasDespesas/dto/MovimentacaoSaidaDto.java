969142e501f65b63cded90ee62158fe519aed112
AndersonRMac <anderson.ramosmaciel@gmail.com>
2024-01-20 10:57:43 -0300

    Criei os DTO's para cada tipo de movimentação;

--- Api_CRUD_Finance/src/main/java/com/minhasDespesas/dto/MovimentacaoSaidaDto.java
+++ Api_CRUD_Finance/src/main/java/com/minhasDespesas/dto/MovimentacaoSaidaDto.java
@@ -3,8 +3,9 @@ package com.minhasDespesas.dto;
 import com.minhasDespesas.model.TipoMovimentacao;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-public record MovimentacaoDto (Long id,
+public record MovimentacaoUsuarioDto (@NotNull Long id,
                               @NotEmpty TipoMovimentacao tipoMovimentacao,
                               @NotBlank String nome,
                               @NotBlank String email,
