6714bcb1bcf5bfe6c47460695c302cd472b1ebb5
AndersonRMac <anderson.ramosmaciel@gmail.com>
2024-01-22 16:14:30 -0300

    Criação do service movimentacao e ajuste do serviceController

--- Api_CRUD_Finance/src/main/java/com/minhasDespesas/controller/MovimentacaoController.java
+++ Api_CRUD_Finance/src/main/java/com/minhasDespesas/controller/MovimentacaoController.java
@@ -3,59 +3,46 @@ package com.minhasDespesas.controller;
 import com.minhasDespesas.dto.MovimentacaoEntradaDto;
 import com.minhasDespesas.dto.MovimentacaoSaidaDto;
 import com.minhasDespesas.model.Movimentacao;
-import com.minhasDespesas.repository.MovimantacaoRepository;
+import com.minhasDespesas.service.MovimentacaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 
 @RestController
 @RequestMapping ("/movimentacao")
 public class MovimentacaoController {
 
     @Autowired
-    MovimantacaoRepository movimantacaoRepository;
+    MovimentacaoService movimentacaoService;
 
     @PostMapping("/entrada")
     public ResponseEntity cadastraEntrada(@RequestBody @Valid MovimentacaoEntradaDto movimentacaoEntradaDto){
-        Movimentacao novaMovimentacao = new Movimentacao(movimentacaoEntradaDto);
-        movimantacaoRepository.save(novaMovimentacao);
+        movimentacaoService.cadastraEntrada(movimentacaoEntradaDto);
         return ResponseEntity.ok("Entrada Cadastrada com Sucesso!!");
     }
 
     @PostMapping("/saida")
     public ResponseEntity cadastraSaida(@RequestBody @Valid MovimentacaoSaidaDto movimentacaoSaidaDto){
-        Movimentacao novaMovimentacao = new Movimentacao(movimentacaoSaidaDto);
-        movimantacaoRepository.save(novaMovimentacao);
+        movimentacaoService.cadastraSaida(movimentacaoSaidaDto);
         return ResponseEntity.ok("Saida Cadastrada com Sucesso!!");
     }
 
     @GetMapping
     public ResponseEntity consultaMovimentacao(){
-        List<Movimentacao> ConsultaMovimentacoes = movimantacaoRepository.findAll();
-        return ResponseEntity.ok(ConsultaMovimentacoes);
+
+        return ResponseEntity.ok(movimentacaoService.consultaMovimentacao());
     }
 
     @PutMapping
     public ResponseEntity alteraMovimentacao(@RequestBody @Valid Movimentacao movimentacao){
-
-        Movimentacao movimentacaoAtualizada = movimantacaoRepository.getReferenceById(movimentacao.getId());
-        movimentacaoAtualizada.setId(movimentacao.getId());
-        movimentacaoAtualizada.setNome(movimentacao.getNome());
-        movimentacaoAtualizada.setValor(movimentacao.getValor());
-        movimentacaoAtualizada.setVencimento(movimentacao.getVencimento());
-        movimantacaoRepository.save(movimentacaoAtualizada);
+        movimentacaoService.alteraMovimentacao(movimentacao);
         return ResponseEntity.ok("Movimentação Alterada com Sucesso!!");
-
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteMovimentacao(@PathVariable Long id){
-        Movimentacao movimentacaoASerDeletada = movimantacaoRepository.getReferenceById(id);
-        movimantacaoRepository.delete(movimentacaoASerDeletada);
+        movimentacaoService.deleteMovimentacao(id);
         return ResponseEntity.ok("Movimentação Excluida com Sucesso!!");
     }
 }
--- Api_CRUD_Finance/src/main/java/com/minhasDespesas/service/MovimentacaoService.java
+++ Api_CRUD_Finance/src/main/java/com/minhasDespesas/service/MovimentacaoService.java
@@ -1,4 +1,51 @@
 package com.minhasDespesas.service;
 
+import com.minhasDespesas.dto.MovimentacaoEntradaDto;
+import com.minhasDespesas.dto.MovimentacaoSaidaDto;
+import com.minhasDespesas.model.Movimentacao;
+import com.minhasDespesas.repository.MovimantacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class MovimentacaoService {
+
+    @Autowired
+    MovimantacaoRepository movimantacaoRepository;
+
+    public Movimentacao cadastraEntrada(MovimentacaoEntradaDto movimentacaoEntradaDto){
+        Movimentacao novaMovimentacao = new Movimentacao(movimentacaoEntradaDto);
+        movimantacaoRepository.save(novaMovimentacao);
+        return novaMovimentacao;
+    }
+
+    public Movimentacao cadastraSaida(MovimentacaoSaidaDto movimentacaoSaidaDto){
+        Movimentacao novaMovimentacao = new Movimentacao(movimentacaoSaidaDto);
+        movimantacaoRepository.save(novaMovimentacao);
+        return novaMovimentacao;
+    }
+
+    public List<Movimentacao> consultaMovimentacao(){
+        List<Movimentacao> consultaMovimentacoes = movimantacaoRepository.findAll();
+        return consultaMovimentacoes;
+    }
+
+    public Movimentacao alteraMovimentacao(Movimentacao movimentacao){
+
+        Movimentacao movimentacaoAtualizada = movimantacaoRepository.getReferenceById(movimentacao.getId());
+        movimentacaoAtualizada.setId(movimentacao.getId());
+        movimentacaoAtualizada.setNome(movimentacao.getNome());
+        movimentacaoAtualizada.setValor(movimentacao.getValor());
+        movimentacaoAtualizada.setVencimento(movimentacao.getVencimento());
+        movimantacaoRepository.save(movimentacaoAtualizada);
+        return movimentacaoAtualizada;
+    }
+
+    public Movimentacao deleteMovimentacao(Long id){
+        Movimentacao movimentacaoASerDeletada = movimantacaoRepository.getReferenceById(id);
+        movimantacaoRepository.delete(movimentacaoASerDeletada);
+        return movimentacaoASerDeletada;
+    }
 }
+
